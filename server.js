b990ce921355517a827c76b200bc9a4878424a52
Ayana Monroe <ayanam@live.unc.edu>
2021-11-22 21:51:21 -0500

    I think this is how I'd fix it

--- server.js
+++ server.js
@@ -27,13 +27,10 @@ app.get("/app/", (req, res, next) => {
 // READ a list of users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {   
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
-	res.status(200).json(stmt);
-});
-
-app.get("/app/users", (req, res) => {   
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").get();
 	res.status(200).json(stmt);
 });
+
 
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
