fb18b0e2cd41afc0feebe357e434c94400aedce1
Ayana Monroe <ayanam@live.unc.edu>
2021-11-22 21:46:32 -0500

    attempt with correct statement for id 2 get

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/app/users", (req, res) => {
 });
 
 app.get("/app/users", (req, res) => {   
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2");
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").get();
 	res.status(200).json(stmt);
 });
 
