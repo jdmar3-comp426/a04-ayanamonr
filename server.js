cc0f58ba55a9a7805d05ce048e8d402874e6c0d0
Ayana Monroe <ayanam@live.unc.edu>
2021-11-30 18:27:16 -0500

    changed to stmt info form

--- server.js
+++ server.js
@@ -21,7 +21,16 @@ app.get("/app/", (req, res, next) => {
 	res.status(200);
 });
 
+
 // Define other CRUD API endpoints using express.js and better-sqlite3
+
+// CREATE a new user (HTTP method POST) at endpoint /app/new/
+
+app.post("/app/new", (req, res) => {
+	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)');
+	const info = stmt.run(req.body.user, md5(req.body.pass));
+	res.status(201).json({"message": info.changes + " record created: ID " + info.lastInsertRowid +" (201)"})
+});
 
 // READ a list of users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {   
@@ -36,15 +45,7 @@ app.get("/app/user/:id", (req, res) => {
 	res.status(200).json(stmt);
 });
 
-// CREATE a new user (HTTP method POST) at endpoint /app/new/
 
-app.post("/app/new", (req, res, next) => {
-	const user = {user: req.body.user, pass: req.body.pass ? md5(req.body.pass): null}  
-	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)');
-	//const info = stmt.run('newtest', '38a7744f5523335db845ff1976bf4747');
-	const output = stmt.run(user.user, user.pass)
-	res.status(201).json({"message": output.changes + " record created: ID " + output.lastInsertRowid +" (201)"})
-});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {  
