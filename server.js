b05e73c18105cad66761223fbddfae6be6bb474a
Ayana Monroe <ayanam@live.unc.edu>
2021-11-30 21:29:15 -0500

    I so lost

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express()
 // Require database SCRIPT file
 var db = require("./database.js")
 // Require md5 MODULE
-var md5 = require('md5')
+var md5 = require("md5")
 // Make Express use its own built-in body parser
 var bodyParser = require("body-parser");
 
